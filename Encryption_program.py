8bc6a2b944971dea6fdc7209e9f05979ce37820f
Aydarbek <mr.aidarbek12@mail.ru>
2024-07-04 18:25:35 +0500

    sd

--- Encryption_program.py
+++ Encryption_program.py
@@ -28,6 +28,6 @@ print(f"encrypted message: {cipher_text}")
 # for letter in cipher_text:
 #     index = key.index(letter)
 #     plain_text += chars[index]
-
-print(f"encrypted message: {cipher_text}")
-print(f"original message : {plain_text}")
+#
+# print(f"encrypted message: {cipher_text}")
+# print(f"original message : {plain_text}")
